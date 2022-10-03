1835a0cb53f112b0b0249e74df825c4eef27ff85
Christopher DiStasio <cadistasio@gmail.com>
2022-10-03 15:37:12 -0500

    Solves all but solution; solves solution in part

--- index.js
+++ index.js
@@ -1,44 +1,84 @@
-function tile(url, left, bottom, width, height){
-    for(let h = 0; h < height; h++){
-        for(let w = 0; w < width; w++){
-            newImage(url, left + w*100, bottom+h*100)
-        }
-    }
+function newImage(objName, url, position, left, bottom) {
+		let object = document.createElement('img')
+		object.alt = objName
+		object.src = url
+		object.style.position = position
+		object.style.left = left + 'px'
+		object.style.bottom = bottom + 'px'
+		document.body.append(object)
+		return object
 }
 
-function newImage(url, left, bottom){
-    let object = document.createElement('img')
-    object.src = url
-    object.style.position = 'fixed'
-    object.style.left = left + 'px'
-    object.style.bottom = bottom + 'px'
-    document.body.append(object)
-    return object
+function newItem (objName, url, position, left, bottom) {
+		let object = newImage(objName, url, position, left, bottom)
+		object.addEventListener('dblclick', () => {
+				object.remove()
+		})
 }
 
-function newItem(url, left, bottom){
-    let object = newImage(url, left, bottom)
+newImage('green character', 'assets/green-character.gif', 'fixed', 100, 100);
+newImage('tree' , 'assets/tree.png', 'fixed', 200, 300)
+newImage('pine tree', 'assets/pine-tree.png', 'fixed', 450, 200)
 
-    object.addEventListener('dblclick', () => {
-        object.remove()
-    })
+newImage('ancient pillar','assets/pillar.png', 'fixed', 350, 100)
+newImage('wooden crate', 'assets/crate.png', 'fixed', 150, 200)
+newImage('well with bucket, rope, and crank', 'assets/well.png', 'fixed', 500, 425)
+
+newItem('sword','assets/sword.png', 'fixed', 500, 405)
+newItem('shield', 'assets/shield.png', 'fixed', 165, 185)
+newItem('wooden staff','assets/staff.png', 'fixed', 600, 100)
+
+// let greenCharacter = document.createElement('img')
+// greenCharacter.src = 'assets/green-character.gif'
+// greenCharacter.style.position = 'fixed'
+// greenCharacter.style.left = '100px'
+// greenCharacter.style.bottom = '100px'
+// document.body.append(greenCharacter)
+
+// let pineTree = document.createElement('img')
+// pineTree.src = 'assets/pine-tree.png'
+// pineTree.style.position = 'fixed'
+// pineTree.style.left = '450px'
+// pineTree.style.bottom = '200px'
+// document.body.append(pineTree)
+
+// let sword = document.createElement('img')
+// sword.src = 'assets/sword.png'
+// sword.position = 'fixed'
+// sword.left = '500px'
+// sword.bottom = '405px'
+// document.body.append(sword)
+
+// sword.addEventListener('click', function(){
+//     sword.remove()
+// })
+
+
+// window.innerHeight
+// 245
+// window.innerWidth
+// 1024
+
+function skyAndGrass(objName, url, position, left, bottom){
+	let object = newImage(objName, url, position, left, bottom)
+		for(let h = 0; h>windowHeight; h++){
+
+
+		}
+		document.body.append(object)
 }
 
-let horizon = window.innerHeight / 1.75
-let heightOfSky = window.innerHeight-horizon
-let heightOfGrass = horizon
+let windowHeight = window.innerHeight
+let windowWidth = window.innerWidth
 
-tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
-tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
+let skyHeight = window.innerHeight-grassHeight
+let grassHeight = window.innerHeight-skyHeight
 
 
-newImage('assets/green-character.gif', 100, 100)
-newImage('assets/tree.png', 200, 300)
-newImage('assets/pillar.png', 350, 100)
-newImage('assets/pine-tree.png', 450, 200)
-newImage('assets/crate.png', 150, 200)
-newImage('assets/well.png', 500, 425)
 
-newItem('assets/sword.png', 500, 405)
-newItem('assets/shield.png', 165, 185)
-newItem('assets/staff.png', 600, 100)
+skyAndGrass('sky', 'assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+skyAndGrass('grass','assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
+
+
+// skyAndGrass('grass', 'assets/grass.png', 'fixed', 0, 0)
+// skyAndGrass('sky', 'assets/sky.png', 'fixed', 0, 425)
